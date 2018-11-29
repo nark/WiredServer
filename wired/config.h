cd2a2f336b1ae57bd913f696f523bdbe0afbea83
Rafaël Warnault <rw@read-write.fr>
2018-11-29 19:23:51 +0100

    Update to debug version 2.4-95

--- wired/config.h
+++ wired/config.h
@@ -92,7 +92,7 @@
 #define WD_VERSION "2.0"
 
 /* Subversion revision */
-#define WI_REVISION "df60014"
+#define WI_REVISION "032539d"
 
 /* 64-bit ino_t on Darwin */
 #define _DARWIN_USE_64_BIT_INODE 1
